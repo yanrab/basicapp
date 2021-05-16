42deed3716dabfef3535afa1201c84ecec01b7ce
yr <yanrab@gmail.com>
2021-05-16 10:19:44 -0400

    more files

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 
 var PORT = process.env.PORT || 5000;
 
-app.get('/*', function (req, res) {
-  res.sendFile(__dirname + req.url);
-});
+
+//app.use('/', express.static(path.join(__dirname + '/webapp')));
+//express.static(path.join(__dirname + '/webapp'));
+
+ app.get('/*', function (req, res) {
+   res.sendFile(__dirname + "/webapp/" + req.url);  
+ });
+
 
 app.listen(PORT, function () {
-  console.log('Basic App listening on port' + PORT);
+  console.log('Basic App listening on port ' + PORT);
 });
